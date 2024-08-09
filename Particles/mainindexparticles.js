7d7f4aca70a1c6c04b716f9849b86521ce34e8e7
maxplayzreal <126032950+maxplayzreal@users.noreply.github.com>
2024-08-09 12:57:58 -0400

    Add files via upload

---
+++ Particles/mainindexparticles.js
@@ -0,0 +1,26 @@
+particlesJS(
+    "cool-particles",
+
+    {
+        particles: {
+            number: {
+                value: 25,
+            },
+            color: {
+                value: "#FF0000"
+            },
+            move: {
+                enable: true,
+                speed: 10
+            },
+            size: {
+                value: 5
+            },
+            line_linked: {
+                enable: true,
+                color: "#fff",
+                width: 2.5
+            }
+        }
+    }
+);
